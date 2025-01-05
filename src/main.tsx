bcc5e49dff2dcb5c700381107972aa8f3e1bd692
Counterfe1t <karoljanbarcz@gmail.com>
2025-01-05 02:03:52 +0100

    Sort & organize all imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.tsx';
 import { BrowserRouter, Route, Routes } from 'react-router';
+import App from './App.tsx';
+import './index.css';
 import Home from './pages/Home.tsx';
+import NotFound from './pages/NotFound.tsx';
 import Products from './pages/Products.tsx';
 import ShoppingLists from './pages/ShoppingLists.tsx';
-import NotFound from './pages/NotFound.tsx';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
